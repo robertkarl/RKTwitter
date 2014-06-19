eb7f1571ff1324c1480c12081692eb9bb58cf045
Robert Karl <robertkarljr@gmail.com>
2014-06-18 23:58:59 -0700

    Tweet item

--- app/src/main/java/com/codepath/apps/RKTwitterClient/TweetArrayAdapter.java
+++ app/src/main/java/com/codepath/apps/RKTwitterClient/TweetArrayAdapter.java
@@ -1,17 +1,17 @@
 package com.codepath.apps.RKTwitterClient;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codepath.apps.RKTwitterClient.models.Tweet;
 
 import java.util.List;
 
-/**
- * Created by androiddev on 6/18/14.
- */
 public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
 
     public TweetArrayAdapter(Context context, List<Tweet> tweets) {
@@ -19,9 +19,23 @@ public class TweetArrayAdapter extends ArrayAdapter<Tweet> {
 
     }
 
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        Tweet tweet = getItem(position);
+        View v;
+        if (convertView == null) {
+            LayoutInflater inflator = LayoutInflater.from(getContext());
+            v = inflator.inflate(R.layout.tweet_item, parent, false);
+        }
+        else {
+            v = convertView;
+        }
+
+        ImageView ivProfileImage = (ImageView)v.findViewById(R.id.ivProfileImage);
+        TextView tvUserName = (TextView) v.findViewById(R.id.tvUserName);
+        TextView tvBody = (TextView) v.findViewById(R.id.tvBody);
+        ivProfileImage.setImageResource(getContext().getResources().getColor(android.R.color.transparent));
+
+        return v;
     }
 }
