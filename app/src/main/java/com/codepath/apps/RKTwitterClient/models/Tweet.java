42e28d36546120fc815bde00203a339f584d63c4
Robert Karl <robertkarljr@gmail.com>
2014-06-19 01:46:47 -0700

    Relative date

--- app/src/main/java/com/codepath/apps/RKTwitterClient/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/RKTwitterClient/models/Tweet.java
@@ -1,16 +1,27 @@
 package com.codepath.apps.RKTwitterClient.models;
 
+import android.text.format.DateUtils;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class Tweet {
     private String body;
     private long uid;
     private String createdAt;
     private User user;
+    private String relativeDate;
+
+    public String getRelativeDate() {
+
+        return relativeDate;
+    }
 
     public String getBody() {
         return body;
@@ -35,6 +46,7 @@ public class Tweet {
             tweet.uid = object.getLong("id");
             tweet.createdAt = object.getString("created_at");
             tweet.user = User.fromJSON(object.getJSONObject("user"));
+            tweet.relativeDate = Tweet.getRelativeTimeAgo(tweet.createdAt);
         }
         catch (JSONException e) {
             e.printStackTrace();
@@ -67,5 +79,22 @@ public class Tweet {
     @Override
     public String toString() {
         return  String.format("%s - %s", getBody(), getUser());
+    }
+
+    public static String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 }
